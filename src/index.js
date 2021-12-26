d01063972c61206a9cbe43b364c8dbb5ee007ac9
Viktor Bondar <vktrbndr@gmail.com>
2021-12-26 23:37:03 +0200

    end

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import './css/styles.css';
+import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
@@ -12,13 +12,21 @@ const debounce = require('lodash.debounce');
 
 let countries = '';
 
+
 function findCountry() {
    
-    countries = input.value;
-    
-    fetchCountries(countries)
-        .then(renderCountriesList)
-    // .catch(error => Notify.failure('Oops, there is no country with that name'))
+    countries = input.value.trim().toLowerCase();
+
+    if (countries === '') {
+        countriesList.innerHTML = '';
+        countryInfo.innerHTML = '';
+        return
+        
+    } else {
+        fetchCountries(countries)
+            .then(renderCountriesList)
+            .catch(error => Notify.failure('Oops, there is no country with that name'));
+    }
     
     
 }
@@ -28,33 +36,48 @@ input.addEventListener('input', debounce(findCountry, DEBOUNCE_DELAY));
 
 
 function renderCountriesList(array) {
-    const countryElList = array.map((country => createListCountreisElement(country))).join('');
-    const countryElListInfo = array.map((country => createCountryInfo(country))).join('');
+    let countryElList = array.map((country => createListCountreisElement(country))).join('');
+    let countryElListInfo = array.map((country => createCountryInfo(country))).join('');
     console.log(countryElList);
-    console.log( countryElListInfo);
-    countriesList.insertAdjacentHTML("afterbegin", countryElList);
-    countriesList.insertAdjacentHTML("afterbegin", countryElListInfo);
+    console.log(countryElListInfo);
+
+    if (array.length > 10) {
+        return Notify.info('Too many matches found. Please enter a more specific name.')
+    }
     
+    if (array.length > 1 && array.length <= 10) {
+        countriesList.innerHTML = countryElList;
+        countryInfo.innerHTML = "";
 
 
+    } else if (array.length === 1) {
+        countriesList.innerHTML = '';
+        countriesList.innerHTML = countryElList;
+        countryInfo.innerHTML = countryElListInfo;
 
-    
+        document.querySelector('.item-box__name').classList.add('item-box__name--uniq');
+        return
+    }
+        
 }
 
 function createListCountreisElement(array) {
     return `<li class='item'>
-    <div> 
-            <img src='${array.flags.svg}' alt='country flag' width = '200px'></img>
-            <p>'${array.name.official}'</p>
+    <div class="item-box"> 
+            <img src='${array.flags.svg}' alt='country flag' width = '25px' class="flag__base"></img>
+            <p class="item-box__name">${array.name.official}</p>
 
     </div> </li>`
 }
 
 function createCountryInfo(array) {
-   return `<ul>
-        <li>Capital: ${array.capital} </li>
-        <li>Population: ${array.population} </li>
-        <li>Languages: ${array.languages} </li>
+    const languages = Object.values(array.languages).join('');
+    console.log(languages);
+
+    return `<ul>
+        <li class="conuntry-info__item"><span>Capital:</span> ${array.capital} </li>
+        <li class="conuntry-info__item"><span>Population:</span> ${array.population} </li>
+        <li class="conuntry-info__item"><span>Languages:</span> ${languages} </li>
     </ul>`
     
 }
